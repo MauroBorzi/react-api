9555a2492e724de05f8219c1fcf15d8b5e47385d
MauroBorzi <mauroborzi91@gmail.com>
2025-09-02 15:50:10 +0200

    [add]milestone 3

--- src/App.jsx
+++ src/App.jsx
@@ -14,21 +14,35 @@ function App() {
 
   return (
     <div className="container">
-      <div className="row">
-        <div className="col-12 col-md-6 col-lg-4">
-          <div className="card">
-            <img src="..." className="card-img-top" alt="..." />
-            <div className="card-body">
-              <h5 className="card-title">nome</h5>
-              <p className="card-text">biografia</p>
-            </div>
-            <ul className="list-group list-group-flush">
-              <li className="list-group-item">anno di nascita</li>
-              <li className="list-group-item">nazionalità</li>
-              <li className="list-group-item">riconoscimenti</li>
-            </ul>
-          </div>
+      <div className="row gy-4">
+        <div className="col-12 my-5 text-center">
+          <h1>BEST ACTRESSES</h1>
+          <p>General information, best movies and awards</p>
         </div>
+        {actresses.map(act => {
+
+          const { id, image, name, biography, birth_year, nationality, awards, most_famous_movies } = act
+
+          return (
+            <div className="col-12 col-md-6 col-lg-3" key={id}>
+              <div className="card h-100">
+                <img src={image} className="card-img-top" alt="..." />
+                <div className="card-body">
+                  <h5 className="card-title">{name}</h5>
+                  <p className="card-text">{biography}</p>
+                </div>
+                <ul className="list-group list-group-flush">
+                  <li className="list-group-item">{birth_year}</li>
+                  <li className="list-group-item">{nationality}</li>
+                  <li className="list-group-item">{awards}</li>
+                  <li className="list-group-item">
+                    <strong>Most famous movies</strong><br />
+                    {most_famous_movies.join(', ')}</li>
+                </ul>
+              </div>
+            </div>
+          )
+        })}
       </div>
     </div>
   )
